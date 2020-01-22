8d5ff0933bd2c6a4b56d4bc5f61e591cd0e81a7d
CA <connorallred.13@gmail.com>
2020-01-22 11:22:18 -0700

    Learned Unity Events

---
+++ Project 1/Assets/Scripts/StartFunction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class StartFunction : MonoBehaviour
+{
+    public float floatNumber;
+    public int scoreValue;
+    public UnityEvent uEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        uEvent.Invoke();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
