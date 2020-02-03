1ac80e70d55b64f953538541ab7e0be46a5a46e9
CA <connorallred.13@gmail.com>
2020-02-03 11:20:49 -0700

    Learned component coding/player controller

---
+++ Project 1/Assets/Scripts/CharacterControls.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public class CharacterControls : MonoBehaviour
+{
+    public CharacterController controller;
+    private Vector3 positionDirection;
+    public float speed = 5f;
+    public float gravity = -3f;
+    public float jumpForce = 6f;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        positionDirection.x = Input.GetAxis("Horizontal")*speed;
+        positionDirection.z = Input.GetAxis("Vertical") * speed;
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            positionDirection.y = jumpForce;
+            
+        }
+        positionDirection.y += gravity;
+        controller.Move(positionDirection*Time.deltaTime);
+    }
+}
